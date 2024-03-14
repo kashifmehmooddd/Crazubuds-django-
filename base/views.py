e8866731828956f523248dcf86dfb2b2cb6bf5a1
kashif mehmood <mehmoodkashif3054@gmail.com>
2024-03-14 14:56:42 +0500

    message topic and User foreign key

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 def home(request):
   return render(request, 'base/home.html', {'rooms': Room.objects.all()})
@@ -7,3 +8,14 @@ def home(request):
 def room(request, id):
   room = Room.objects.get(pk=id)
   return render(request, 'base/room.html', {'room': room})
+
+def createRoom(request):
+  form = RoomForm()
+  if request.method == 'POST':
+    form = RoomForm(request.POST)
+    if form.is_valid:
+      form.save()
+      return redirect('home')
+
+  context={ "form": form }
+  return render(request, 'base/room_form.html', context)
